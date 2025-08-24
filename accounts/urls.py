81a5886a4d1255fa9bf26c54aeaf0bfb13c71d81
rahul678908 <rahulkannan33886@gmail.com>
2025-08-24 21:15:29 +0530

    completed version 2.0

--- accounts/urls.py
+++ accounts/urls.py
@@ -46,6 +46,7 @@ urlpatterns = [
     path("about-us/", views.about_us_list, name="about-us-list"),
     path('about-us/add/', views.about_us_add, name='about-us-add'),
     path("about-us/image-delete/<int:pk>/", views.about_us_image_delete, name="about-us-image-delete"),
+    
 
     path('register-customer/', views.register_customer, name='register_customer'),
     path('customer-login/', views.customer_login, name='customer-login'),
